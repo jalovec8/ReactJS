71435791c6fe5a402f5b6bd4b7d5a89f8f290ea0
Jan Jalovec <janjalovec@hotmail.com>
2023-11-15 18:05:26 +0100

    GPT Code

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+//import React from 'react';
+//import './App.css';
+
+//function App() {
+// return (<div>
+
+
+ //    <h1>Honza</h1>
+ // </div>);
+ 
+//}
+//export default App;
+
+// App.js
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
- return (<div>
+  const [text, setText] = useState('Honza');
 
+  const changeText = () => {
+    setText('Nový text!');
+    setText('Ahoj');
+    setText('Zdravím');
+  };
 
-     <h1>Honza</h1>
-  </div>);
- 
+  return (
+    <div>
+      <h1>{text}</h1>
+      <button onClick={changeText}>Změnit text</button>
+    </div>
+  );
 }
+
 export default App;
+
